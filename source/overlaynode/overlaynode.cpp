db9660468f67eb36f78c5d5f6e36ee31c19b336e
fink pan <hspan1234@gmail.com>
2016-03-23 10:44:49 +0800

    3-23am

--- source/overlaynode/overlaynode.cpp
+++ source/overlaynode/overlaynode.cpp
@@ -3,8 +3,12 @@
 
 #include <osgViewer/Viewer>
 #include <osgViewer/CompositeViewer>
+#include <osgViewer/ViewerEventHandlers>
+
+#include <osgQt/GraphicsWindowQt>
 
 #include <osgGA/TrackballManipulator>
+#include <osgGA/MultiTouchTrackballManipulator>
 
 #include <osg/Material>
 #include <osg/Geode>
@@ -20,6 +24,11 @@
 #include <osgDB/Registry>
 
 #include <osgSim/OverlayNode>
+
+#include <QWidget>
+#include <QTimer>
+#include <QApplication>
+#include <QGridLayout>
 
 #include "draw_line.hpp"
 
@@ -116,3 +125,103 @@ osg::Node* createLine(const osg::Vec3& p1, const osg::Vec3& p2)
 // #endif
 // 
 // }
+
+class ViewerWidget: public QWidget,public osgViewer::CompositeViewer
+{
+public:
+  ViewerWidget(QWidget* parent = 0,Qt::WindowFlags f = 0,osgViewer::ViewerBase::ThreadingModel threadingModel=osgViewer::CompositeViewer::SingleThreaded): QWidget(parent,f)
+  {
+    setThreadingModel(threadingModel);
+
+    // disable the default setting of viewer.done() by pressing Escape.
+    setKeyEventSetsDone(0);
+
+
+    osg::ref_ptr<osg::Node> line = createLine(osg::Vec3(-2,0,2),osg::Vec3(2,0,2));
+    osgSim::OverlayNode* overlayNode = new osgSim::OverlayNode(
+      osgSim::OverlayNode::OBJECT_DEPENDENT_WITH_ORTHOGRAPHIC_OVERLAY);
+    overlayNode->setContinuousUpdate(true);
+    overlayNode->setOverlaySubgraph(line);
+    overlayNode->setOverlayBaseHeight(0.99);
+    osg::ref_ptr<osg::Node> cube = osgDB::readNodeFile("cube.osgb");
+    overlayNode->addChild(cube);
+    osg::ref_ptr<osg::Group> root = new osg::Group;
+    root->addChild(overlayNode);
+    root->addChild(line);
+
+    QWidget* widget1 = addViewWidget( createGraphicsWindow(0,0,100,100), osgDB::readRefNodeFile("cow.osgt") );
+//     QWidget* widget1 = addViewWidget(createGraphicsWindow(0,0,100,100),root);
+    QWidget* widget2 = addViewWidget(createGraphicsWindow(0,0,100,100),osgDB::readRefNodeFile("glider.osgt"));
+    QWidget* widget3 = addViewWidget(createGraphicsWindow(0,0,100,100),osgDB::readRefNodeFile("axes.osgt"));
+    QWidget* widget4 = addViewWidget(createGraphicsWindow(0,0,100,100),osgDB::readRefNodeFile("fountain.osgt"));
+    QWidget* popupWidget = addViewWidget(createGraphicsWindow(900,100,320,240,"Popup window",true),osgDB::readRefNodeFile("dumptruck.osgt"));
+    popupWidget->show();
+
+    QGridLayout* grid = new QGridLayout;
+    grid->addWidget(widget1,0,0);
+    grid->addWidget(widget2,0,1);
+    grid->addWidget(widget3,1,0);
+    grid->addWidget(widget4,1,1);
+    setLayout(grid);
+
+    connect(&_timer,SIGNAL(timeout()),this,SLOT(update()));
+    _timer.start(10);
+  }
+
+  QWidget* addViewWidget(osgQt::GraphicsWindowQt* gw,osg::ref_ptr<osg::Node> scene)
+  {
+    osgViewer::View* view = new osgViewer::View;
+    addView(view);
+
+    osg::Camera* camera = view->getCamera();
+    camera->setGraphicsContext(gw);
+
+    const osg::GraphicsContext::Traits* traits = gw->getTraits();
+
+    camera->setClearColor(osg::Vec4(0.2,0.2,0.6,1.0));
+    camera->setViewport(new osg::Viewport(0,0,traits->width,traits->height));
+    camera->setProjectionMatrixAsPerspective(30.0f,static_cast<double>(traits->width)/static_cast<double>(traits->height),1.0f,10000.0f);
+
+    view->setSceneData(scene);
+    view->addEventHandler(new osgViewer::StatsHandler);
+    view->setCameraManipulator(new osgGA::MultiTouchTrackballManipulator);
+    gw->setTouchEventsEnabled(true);
+    return gw->getGLWidget();
+  }
+
+  osgQt::GraphicsWindowQt* createGraphicsWindow(int x,int y,int w,int h,const std::string& name="",bool windowDecoration=false)
+  {
+    osg::DisplaySettings* ds = osg::DisplaySettings::instance().get();
+    osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
+    traits->windowName = name;
+    traits->windowDecoration = windowDecoration;
+    traits->x = x;
+    traits->y = y;
+    traits->width = w;
+    traits->height = h;
+    traits->doubleBuffer = true;
+    traits->alpha = ds->getMinimumNumAlphaBits();
+    traits->stencil = ds->getMinimumNumStencilBits();
+    traits->sampleBuffers = ds->getMultiSamples();
+    traits->samples = ds->getNumMultiSamples();
+
+    return new osgQt::GraphicsWindowQt(traits.get());
+  }
+
+  virtual void paintEvent(QPaintEvent* event)
+  { frame(); }
+
+protected:
+
+  QTimer _timer;
+};
+
+int main(int argc,char** argv)
+{
+
+  QApplication app(argc,argv);
+  ViewerWidget* viewWidget = new ViewerWidget(0,Qt::Widget,osgViewer::ViewerBase::SingleThreaded);
+  viewWidget->setGeometry(100,100,800,600);
+  viewWidget->show();
+  return app.exec();
+}
